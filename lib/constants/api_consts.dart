dec7d60cebe7912acac488e31cad0c30d8e3df4f
Muhammad Bilal <89094665+mbilal52@users.noreply.github.com>
2024-06-04 09:22:44 +0500

    Update api_consts.dart

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,9 +1,6 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "YOUR OWN API KEY";
 
-
-// String API_KEY = "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";
-//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
 
 
 
